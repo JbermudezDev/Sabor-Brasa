662b6ad8c2c3c8d7be5a9a17771488884697e359
BermuGod28 <thejuanjo1128@gmail.com>
2025-03-04 12:14:25 -0500

    Boton ver sirviendo

--- demo/src/main/java/com/example/demo/controlador/ClienteController.java
+++ demo/src/main/java/com/example/demo/controlador/ClienteController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/clientes")
@@ -24,6 +25,17 @@ public class ClienteController {
         return "ListadoClientes";
     }
 
+    @GetMapping("/view/{id}")
+    public String verCliente(@PathVariable("id") Long id, Model model) {
+        Optional<Cliente> cliente = clienteService.findById(id);
+        if (cliente.isPresent()) {
+            model.addAttribute("cliente", cliente.get());
+            return "VerCliente";
+        } else {
+            return "redirect:/clientes/all";
+        }
+    }
+
     @PostMapping("/add")
     public String agregarCliente(@ModelAttribute Cliente cliente) {
         clienteService.add(cliente);
@@ -33,7 +45,7 @@ public class ClienteController {
     @DeleteMapping("/delete/{id}")
     public String eliminarCliente(@PathVariable("id") Long id) {
         clienteService.deleteById(id);
-        return "redirect:/clientes/all";
+        return "redirect:/ListadoClientes";
     }
 
     @PutMapping("/update/{id}")
