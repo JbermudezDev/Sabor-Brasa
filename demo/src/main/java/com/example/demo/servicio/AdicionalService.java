d7da7ee8cfdfdf70d81a1e9d73508e713847dc3a
alejandro09pf <alejandro09pf@hotmail.com>
2025-03-06 00:25:11 -0500

    CRUD Adicionales

---
+++ demo/src/main/java/com/example/demo/servicio/AdicionalService.java
@@ -0,0 +1,41 @@
+package com.example.demo.servicio;
+
+import com.example.demo.entidades.Adicional;
+import com.example.demo.repositorio.AdicionalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AdicionalService {
+
+    @Autowired
+    private AdicionalRepository adicionalRepository;
+
+    // Metodo para obtener todos los adicionales
+    public List<Adicional> searchAll() {
+        return adicionalRepository.findAll(); // Llamada al repositorio para obtener todos los adicionales
+    }
+
+    // Método para agregar un adicional
+    public void add(Adicional adicional) {
+        adicionalRepository.save(adicional); // Guardar el adicional en la base de datos
+    }
+
+    // Método para eliminar un adicional por su ID
+    public void deleteById(Long id) {
+        adicionalRepository.deleteById(id); // Eliminar adicional por ID
+    }
+
+    // Método para actualizar un adicional
+    public void update(Adicional adicional) {
+        adicionalRepository.save(adicional); // Guardar el adicional actualizado
+    }
+
+    // Método para buscar un adicional por su ID
+    public Optional<Adicional> findById(Long id) {
+        return adicionalRepository.findById(id); // Buscar adicional por ID
+    }
+}
