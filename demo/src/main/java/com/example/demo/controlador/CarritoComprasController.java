5b9a57a074634aad112b9c4545eb15f903490a6b
paula2704 <95259154+paula2704@users.noreply.github.com>
2025-04-29 02:46:50 -0500

    Carrito de compras

--- demo/src/main/java/com/example/demo/controlador/CarritoComprasController.java
+++ demo/src/main/java/com/example/demo/controlador/CarritoComprasController.java
@@ -5,16 +5,28 @@ import com.example.demo.servicio.CarritoComprasService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/carrito")
 @CrossOrigin(origins = "http://localhost:4200") // Permitir solicitudes desde Angular
 public class CarritoComprasController {
 
-    
+  @Autowired
+  private CarritoComprasService carritoComprasService;
+
+  @PostMapping("/guardar")
+  public ResponseEntity<?> guardarCarrito(@RequestBody CarritoCompras carrito) {
+    try {
+      CarritoCompras carritoGuardado = carritoComprasService.guardar(carrito);
+      return ResponseEntity.ok(carritoGuardado);
+    } catch (Exception e) {
+      return ResponseEntity
+        .badRequest()
+        .body("Error al guardar el carrito: " + e.getMessage());
+    }
+  }
 }
--- demo/src/main/java/com/example/demo/servicio/CarritoComprasService.java
+++ demo/src/main/java/com/example/demo/servicio/CarritoComprasService.java
@@ -4,14 +4,18 @@ import com.example.demo.entidades.CarritoCompras;
 import com.example.demo.entidades.SeleccionarProductos;
 import com.example.demo.repositorio.CarritoComprasRepository;
 import com.example.demo.repositorio.SeleccionarProductosRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class CarritoComprasService {
 
-    
+  @Autowired
+  private CarritoComprasRepository carritoComprasRepository;
+
+  public CarritoCompras guardar(CarritoCompras carrito) {
+    return carritoComprasRepository.save(carrito);
+  }
 }
