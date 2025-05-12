85a6fdd8186a8f1b657da6708a96e979ee45640c
paula2704 <95259154+paula2704@users.noreply.github.com>
2025-05-11 20:21:52 -0500

    Pruebas

--- demo/src/test/java/com/example/demo/e2e/UseCaseTest.java
+++ demo/src/test/java/com/example/demo/e2e/UseCaseTest.java
@@ -2,11 +2,15 @@ package com.example.demo.e2e;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import java.time.Duration;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
@@ -15,7 +19,6 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ActiveProfiles("test")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-
 public class UseCaseTest {
 
   private final String BASE_URL = "http://localhost:4200";
@@ -39,6 +42,34 @@ public class UseCaseTest {
 
   @Test
   public void UseCaseOne() {
-    driver.get("http://localhost:4200/");
+    driver.get(BASE_URL);
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.id("btnLogin")));
+    WebElement btnElement = driver.findElement(By.id("btnLogin"));
+    btnElement.click();
+
+    WebElement emailInput = wait.until(
+      ExpectedConditions.elementToBeClickable(By.id("email"))
+    );
+    WebElement passwordInput = driver.findElement(By.id("password"));
+
+    emailInput.sendKeys("7Ig6a@example.com");
+    passwordInput.sendKeys("1234");
+
+    WebElement btnIniciar = driver.findElement(By.id("btnIniciar"));
+    btnIniciar.click();
+
+    emailInput.clear();
+    passwordInput.clear();
+
+    emailInput.sendKeys("thejuanjo1128@gmail.com");
+    passwordInput.sendKeys("123456");
+
+    WebElement btnIniciar2 = driver.findElement(By.id("btnIniciar"));
+    btnIniciar2.click();
+  }
+
+  @AfterEach
+  public void tearDown() {
+    //driver.quit();
   }
 }
